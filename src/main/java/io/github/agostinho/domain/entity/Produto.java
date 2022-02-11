66ab8c9c0f78825a7f7b30a64aedfbc4d0eb24fd
Agostinho Neto <agostneto6@gmail.com>
2022-02-11 16:40:21 -0300

    Comentário do commit da minha nova branch

---
+++ src/main/java/io/github/agostinho/domain/entity/Produto.java
@@ -0,0 +1,4 @@
+package io.github.agostinho.domain.entity;
+
+public class Produto {
+}
--- src/main/java/io/github/agostinho/repository/ClientesRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.agostinho.repository;
-
-import io.github.agostinho.model.Cliente;
-
-public class ClientesRepository {
-    public void persistir(Cliente cliente) {
-        //acessa a base e salvar o cliente
-    }
-}
