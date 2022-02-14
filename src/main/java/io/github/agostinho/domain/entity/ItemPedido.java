fe30a6f7f92951051c1144ffc8edcdd53e0c9187
Agostinho Neto <agostneto6@gmail.com>
2022-02-14 18:15:00 -0300

    Refatoração do Codigo

--- src/main/java/io/github/agostinho/domain/entity/ItemPedido.java
+++ src/main/java/io/github/agostinho/domain/entity/ItemPedido.java
@@ -1,2 +1,56 @@
-package io.github.agostinho.domain.entity;public class ItemPedido {
+/*
+ * Copyright (C) 2022 REPLACE_WITH_NAME
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.agostinho.domain.entity;
+
+public class ItemPedido {
+    private Integer id;
+    private Pedido pedido;
+    private Produto produto;
+    private Integer quantidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
 }
