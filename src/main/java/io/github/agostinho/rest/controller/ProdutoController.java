06cefddc68c7008194a558976afad0374b94ffa2
Agostinho Neto <agostneto6@gmail.com>
2022-02-19 16:22:16 -0300

    ApiRest completo Produto

--- src/main/java/io/github/agostinho/rest/controller/ProdutoController.java
+++ src/main/java/io/github/agostinho/rest/controller/ProdutoController.java
@@ -15,10 +15,18 @@
  */
 
 package io.github.agostinho.rest.controller;
+import io.github.agostinho.domain.entity.Cliente;
 import io.github.agostinho.domain.entity.Produto;
 import io.github.agostinho.domain.repository.Produtos;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -28,9 +36,56 @@ public class ProdutoController {
     public ProdutoController(Produtos repository) {
         this.repository = repository;
     }
+
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(CREATED)
     public Produto save(@RequestBody Produto produto){
         return repository.save(produto);
     }
+
+    @PutMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void update(@PathVariable Integer id,@RequestBody Produto produto){
+        repository.findById(id)
+                .map(p->{
+                    produto.setId(p.getId());
+                    repository.save(produto);
+                    return produto;
+                }).orElseThrow(()->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Produto não encontrado"));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository.findById(id)
+                .map(p->{
+                    repository.delete(p);
+                    return Void.TYPE;
+                }).orElseThrow(()->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Produto não encontrado"));
+    }
+
+    @GetMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public Produto getById(@PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow(() ->new ResponseStatusException(NOT_FOUND,
+                        "Produto não encontrado"));
+    }
+
+    @GetMapping
+    public List<Produto> find(Produto filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(
+                        ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        return repository.findAll(example);
+    }
+
 }
